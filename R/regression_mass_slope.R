0a86dccfe3446c5ce739ba155e086cdf62518598
Kristina Riemer <kristina.riemer@weecology.org>
2018-12-18 16:29:56 -0500

    Add new function for calculating yearly mass slopes

---
+++ R/regression_mass_slope.R
@@ -0,0 +1,29 @@
+#' Calculation of each species' annual mass slope
+#' 
+#' Takes annual masses for all species, runs linear regression on all annual
+#' masses for each species, and extracts slope
+#' 
+#' @param mass_df dataframe containing all annual masses for all species
+#' 
+#' @return dataframe with species and slope columns
+#' 
+#' @examples 
+#' df = data.frame(species = c("A", "A", "A"), yr = c(1990, 1991, 1992), mass_mean = c(21, 23, 25))
+#' regression_mass_slope(df)
+#' 
+#' @export
+#' @import dplyr
+#' @import tidyr
+#' @import broom
+#' @import purrr
+
+regression_mass_slope = function(mass_df){
+  regression_df = mass_df %>% 
+    nest(-species) %>% 
+    mutate(fit = map(data, ~lm(mass_mean ~ yr, data =.)), 
+           results = map(fit, tidy)) %>% 
+    unnest(results) %>% 
+    filter(term == "yr") %>% 
+    select(species, slope = estimate)
+  return(regression_df)
+}

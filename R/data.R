5fdb682bc36080fefa4540d74c5720732a5d9d3c
Kristina Riemer <kristina.riemer@weecology.org>
2018-12-21 15:24:30 -0500

    Update example dataset documentation

--- R/data.R
+++ R/data.R
@@ -3,8 +3,12 @@
 #' This is a made up set of data for three species at two sites to ensure the
 #' threshold_mass function is working properly. 
 #' 
-#' @format A dataframe with xx rows and xx variables: 
+#' @format A data frame with 48 rows and 5 variables:
 #' \describe{
-#' \item{col}{description}
+#' \item{species}{species classification for individual}
+#' \item{inds}{number of individuals of that species in dataset}
+#' \item{yr}{year that individual was collected}
+#' \item{location}{site where that individual was collected}
+#' \item{avg_temp}{mean temperature for the year that the individual was collected}
 #' }
 "threshold_mass_data"

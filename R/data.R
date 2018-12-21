f11ec7241ef488254a5be7fa2dd0dbaed2374571
Kristina Riemer <kristina.riemer@weecology.org>
2018-12-21 13:04:47 -0500

    Add new example dataset of individual masses to test new function

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Example dataset for testing mass data thresholding function
+#' 
+#' This is a made up set of data for three species at two sites to ensure the
+#' threshold_mass function is working properly. 
+#' 
+#' @format A dataframe with xx rows and xx variables: 
+#' \describe{
+#' \item{col}{description}
+#' }
+"threshold_mass_data"
